9d9a6c823e6d025bb2630d8a8defffb781c6c009
Kunal Gupta <kunal.gupta@appdynamics.com>
2017-03-06 12:07:59 -0800

    - Fixed java path issue

--- src/main/resources/logstash-alert.bat
+++ src/main/resources/logstash-alert.bat
@@ -1,3 +1,3 @@
 @echo off
 
-..\..\..\jre\bin\java -Dlog4j.configuration=file:conf/log4j.xml -jar logstash-alert-extension.jar %*
+java -Dlog4j.configuration=file:conf/log4j.xml -jar logstash-alert-extension.jar %*
--- src/main/resources/logstash-alert.sh
+++ src/main/resources/logstash-alert.sh
@@ -1,1 +1,1 @@
-../../../jdk/bin/java -Dlog4j.configuration=file:conf/log4j.xml -jar logstash-alert-extension.jar "$@"
+java -Dlog4j.configuration=file:conf/log4j.xml -jar logstash-alert-extension.jar "$@"
